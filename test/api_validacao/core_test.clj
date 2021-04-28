37f023d00c196340994e09ef3a67aebf81450e5a
Alex Junior <alexmomijunior@gmail.com>
2021-04-28 10:34:09 -0300

    configuraçao inicial do projeto

---
+++ test/api_validacao/core_test.clj
@@ -0,0 +1,7 @@
+(ns api-validacao.core-test
+  (:require [clojure.test :refer :all]
+            [api-validacao.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
