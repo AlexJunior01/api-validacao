8b073e0382f22d5a1261c4292a2af5c5d361e26f
Alex Junior <alexmomijunior@gmail.com>
2021-04-28 13:47:54 -0300

    first API config

---
+++ src/api_validacao/web.clj
@@ -0,0 +1,73 @@
+(ns api-validacao.web
+  (:require [reitit.ring :as ring]
+            [reitit.coercion.spec]
+            [reitit.swagger :as swagger]
+            [reitit.swagger-ui :as swagger-ui]
+            [reitit.ring.coercion :as coercion]
+            [reitit.dev.pretty :as pretty]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [reitit.ring.middleware.exception :as exception]
+            [reitit.ring.middleware.multipart :as multipart]
+            [reitit.ring.middleware.parameters :as parameters]
+            [reitit.ring.middleware.dev :as dev]
+            [reitit.ring.spec :as spec]
+            [spec-tools.spell :as spell]
+            [ring.adapter.jetty :as jetty]
+            [muuntaja.core :as m]
+            [clojure.java.io :as io]))
+
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/swagger.json"
+        {:get {:no-doc true
+               :swagger {:info {:title "Validation API"
+                                :description "API for validation of documents"}}
+               :handler (swagger/create-swagger-handler)}}]
+
+       ["/api"
+        {:swagger {:tags ["api"]}}
+        ["/healthcheck" {:get {:summary "Check if is online"
+                         :responses {200 {:body {:message string?}}}
+                         :handler (fn [_] {:status 200
+                                           :body {:message "The API is working!!"}})}}]]]
+
+
+      {:exception pretty/exception
+       :data {:coercion reitit.coercion.spec/coercion
+              :muuntaja m/instance
+              :middleware [;; swagger feature
+                           swagger/swagger-feature
+                           ;; query-params & form-params
+                           parameters/parameters-middleware
+                           ;; content-negotiation
+                           muuntaja/format-negotiate-middleware
+                           ;; encoding response body
+                           muuntaja/format-response-middleware
+                           ;; exception handling
+                           exception/exception-middleware
+                           ;; decoding request body
+                           muuntaja/format-request-middleware
+                           ;; coercing response bodys
+                           coercion/coerce-response-middleware
+                           ;; coercing request parameters
+                           coercion/coerce-request-middleware
+                           ;; multipart
+                           multipart/multipart-middleware]}})
+
+
+
+      (ring/routes
+        (swagger-ui/create-swagger-ui-handler
+          {:path "/docs"
+           :config {:validatorUrl nil
+                    :operationSorter "alpha"}})
+        (ring/create-default-handler))))
+
+
+(defn start []
+  (jetty/run-jetty #'app {:port 3000, :join? false})
+  (println "server running in port 3000"))
+
+(comment (start))
