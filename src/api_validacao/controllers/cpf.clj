5241904e18d2a6422b194576d8c9034bcc9c8f2d
Alex Junior <alexmomijunior@gmail.com>
2021-04-29 13:43:28 -0300

    Gerar cpf com ou sem pontos

---
+++ src/api_validacao/controllers/cpf.clj
@@ -0,0 +1,47 @@
+(ns api-validacao.controllers.cpf)
+
+
+(defn first-digit
+  [nine-digits-cpf]
+  (let [remainder (->> nine-digits-cpf
+                       (mapv * (range 10 1 -1))
+                       (reduce +))
+        remainder (mod remainder 11)]
+
+    (if (or (= remainder 0) (= remainder 1))
+      0
+      (- 11 remainder))))
+
+(defn second-digit
+  [ten-digits-cpf]
+  (let [remainder (->> ten-digits-cpf
+                       (mapv * (range 11 1 -1))
+                       (reduce +))
+        remainder (mod remainder 11)]
+
+    (if (or (= remainder 0) (= remainder 1))
+      0
+      (- 11 remainder))))
+
+
+(defn mask-cpf
+  [cpf]
+  (str (subs cpf 0 3) "." (subs cpf 3 6) "." (subs cpf 6 9) "-" (subs cpf 9 11)))
+
+
+(defn generate-cpf!
+  []
+  (let [nine-digits (repeatedly 10 #(rand-int 10))
+        cpf (conj nine-digits (first-digit nine-digits))
+        cpf (conj cpf (second-digit cpf))]
+    (reduce str cpf)))
+
+
+(defn handle-cpf-generate
+  [body]
+  (let [cpf (generate-cpf!)]
+    (if (:with_dots body)
+      {:body {:cpf     (mask-cpf cpf)
+              :message "CPF gerado com sucesso"}}
+      {:body {:cpf     cpf
+              :message "CPF gerado com sucesso"}})))
--- src/api_validacao/routes/cpf.clj
+++ src/api_validacao/routes/cpf.clj
@@ -1,33 +1,6 @@
-(ns api-validacao.routes.cpf)
-
-(defn first-digit
-  [nine-digits-cpf]
-  (let [remainder (->> nine-digits-cpf
-                  (mapv * (range 10 1 -1))
-                  (reduce +))
-        remainder (mod remainder 11)]
-
-    (if (or (= remainder 0) (= remainder 1))
-      0
-      (- 11 remainder))))
-
-(defn second-digit
-  [ten-digits-cpf]
-  (let [remainder (->> ten-digits-cpf
-                  (mapv * (range 11 1 -1))
-                  (reduce +))
-        remainder (mod remainder 11)]
-
-    (if (or (= remainder 0) (= remainder 1))
-      0
-      (- 11 remainder))))
-
-(defn generate-cpf!
-  [body]
-  (let [nine-digits (repeatedly 10 #(rand-int 10))
-        cpf (conj nine-digits (first-digit nine-digits))
-        cpf (conj cpf (second-digit cpf))]
-    (reduce str cpf)))
+(ns api-validacao.routes.cpf
+  (:require [api-validacao.controllers.cpf :as cpf]
+            [clojure.string :as str]))
 
 
 (def routes
@@ -38,8 +11,6 @@
                        :responses  {200 {:body {:cpf     string?
                                                 :message string?}}}
                        :handler    (fn [{{:keys [query]} :parameters}]
-                                     (generate-cpf! query))}}]
+                                     (cpf/handle-cpf-generate query))}}]
    ])
 
-
-(comment (generate-cpf {:teste "teste"}))
